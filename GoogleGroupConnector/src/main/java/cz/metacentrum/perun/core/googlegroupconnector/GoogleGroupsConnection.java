655e70a0e3a6bf1bb70e8e38426e2cea83b37d20
Pavel Zlámal <zlamal@cesnet.cz>
2017-06-09 07:41:54 +0200

    Updated to Java 8
    - Sources marked as Java 8 for jar plugin.
    - Project moved to cz.metacentrum.perun group in pom.xml.
    - Spaces to tabs.
    - Fixed readme.

--- GoogleGroupConnector/src/main/java/cz/metacentrum/perun/core/googlegroupconnector/GoogleGroupsConnection.java
+++ GoogleGroupConnector/src/main/java/cz/metacentrum/perun/core/googlegroupconnector/GoogleGroupsConnection.java
@@ -32,149 +32,149 @@ import org.slf4j.LoggerFactory;
  */
 public class GoogleGroupsConnection {
 
-    /**
-     * Path to properties file.
-     */
-    private static String PROPERTIES_PATH;
+	/**
+	 * Path to properties file.
+	 */
+	private static String PROPERTIES_PATH;
 
-    /**
-     * Application name.
-     */
-    private static final String APPLICATION_NAME = "Google Groups Perun Service";
+	/**
+	 * Application name.
+	 */
+	private static final String APPLICATION_NAME = "Google Groups Perun Service";
 
-    /**
-     * Global instance of the JSON factory.
-     */
-    private static JsonFactory JSON_FACTORY;
+	/**
+	 * Global instance of the JSON factory.
+	 */
+	private static JsonFactory JSON_FACTORY;
 
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static HttpTransport HTTP_TRANSPORT;
+	/**
+	 * Global instance of the HTTP transport.
+	 */
+	private static HttpTransport HTTP_TRANSPORT;
 
-    /**
-     * Global instance of the scopes required by this application.
-     */
-    private static List<String> SCOPES;
+	/**
+	 * Global instance of the scopes required by this application.
+	 */
+	private static List<String> SCOPES;
 
-    /**
-     * Email of the Service Account.
-     */
-    private static String SERVICE_ACCOUNT_EMAIL;
+	/**
+	 * Email of the Service Account.
+	 */
+	private static String SERVICE_ACCOUNT_EMAIL;
 
-    /**
-     * Email of the User that Application will work behalf on.
-     */
-    private static String USER_EMAIL;
+	/**
+	 * Email of the User that Application will work behalf on.
+	 */
+	private static String USER_EMAIL;
 
-    /**
-     * Path to the Service Account Private Key file.
-     *
-     * Generated at https://console.developers.google.com/project according to
-     * https://developers.google.com/identity/protocols/OAuth2ServiceAccount#creatinganaccount
-     */
-    private static String SERVICE_ACCOUNT_PKCS12_FILE_PATH;
+	/**
+	 * Path to the Service Account Private Key file.
+	 *
+	 * Generated at https://console.developers.google.com/project according to
+	 * https://developers.google.com/identity/protocols/OAuth2ServiceAccount#creatinganaccount
+	 */
+	private static String SERVICE_ACCOUNT_PKCS12_FILE_PATH;
 
-    private final static org.slf4j.Logger log = LoggerFactory.getLogger(GoogleGroupsConnection.class);
+	private final static org.slf4j.Logger log = LoggerFactory.getLogger(GoogleGroupsConnection.class);
 
-    public GoogleGroupsConnection(String domainFile) throws IOException, GeneralSecurityException {
-        GoogleGroupsConnection.PROPERTIES_PATH = domainFile;
-        this.loadProperties();
-    }
+	public GoogleGroupsConnection(String domainFile) throws IOException, GeneralSecurityException {
+		GoogleGroupsConnection.PROPERTIES_PATH = domainFile;
+		this.loadProperties();
+	}
 
-    /**
-     * Returns variable containing domain name.
-     *
-     * @return DOMAIN_NAME name of the domain in Google Groups
-     */
-    public String getDomainName() {
-        return USER_EMAIL.substring(USER_EMAIL.indexOf("@") + 1);
-    }
+	/**
+	 * Returns variable containing domain name.
+	 *
+	 * @return DOMAIN_NAME name of the domain in Google Groups
+	 */
+	public String getDomainName() {
+		return USER_EMAIL.substring(USER_EMAIL.indexOf("@") + 1);
+	}
 
-    /**
-     * Loads properties and sets static class variables.
-     *
-     */
-    private void loadProperties() throws IOException, GeneralSecurityException {
+	/**
+	 * Loads properties and sets static class variables.
+	 *
+	 */
+	private void loadProperties() throws IOException, GeneralSecurityException {
 
-        Properties prop = new Properties();
-        InputStream input = null;
+		Properties prop = new Properties();
+		InputStream input = null;
 
-        try {
-            input = new FileInputStream(PROPERTIES_PATH);
+		try {
+			input = new FileInputStream(PROPERTIES_PATH);
 
-            // load a properties file
-            prop.load(input);
+			// load a properties file
+			prop.load(input);
 
-            // store values from properties file in static variables
-            GoogleGroupsConnection.SERVICE_ACCOUNT_EMAIL = prop.getProperty("service_account_email");
-            GoogleGroupsConnection.USER_EMAIL = prop.getProperty("user_email");
-            GoogleGroupsConnection.SERVICE_ACCOUNT_PKCS12_FILE_PATH = prop.getProperty("service_account_pkcs12_file_path");
-            GoogleGroupsConnection.JSON_FACTORY = JacksonFactory.getDefaultInstance();
-            GoogleGroupsConnection.HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            GoogleGroupsConnection.SCOPES = Arrays.asList(DirectoryScopes.ADMIN_DIRECTORY_USER,
-                    DirectoryScopes.ADMIN_DIRECTORY_USER_READONLY,
-                    DirectoryScopes.ADMIN_DIRECTORY_GROUP,
-                    DirectoryScopes.ADMIN_DIRECTORY_GROUP_MEMBER);
+			// store values from properties file in static variables
+			GoogleGroupsConnection.SERVICE_ACCOUNT_EMAIL = prop.getProperty("service_account_email");
+			GoogleGroupsConnection.USER_EMAIL = prop.getProperty("user_email");
+			GoogleGroupsConnection.SERVICE_ACCOUNT_PKCS12_FILE_PATH = prop.getProperty("service_account_pkcs12_file_path");
+			GoogleGroupsConnection.JSON_FACTORY = JacksonFactory.getDefaultInstance();
+			GoogleGroupsConnection.HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+			GoogleGroupsConnection.SCOPES = Arrays.asList(DirectoryScopes.ADMIN_DIRECTORY_USER,
+					DirectoryScopes.ADMIN_DIRECTORY_USER_READONLY,
+					DirectoryScopes.ADMIN_DIRECTORY_GROUP,
+					DirectoryScopes.ADMIN_DIRECTORY_GROUP_MEMBER);
 
-        } catch (IOException ex) {
-            String msg = "Problem with I/O operation while reading google_groups.properties file.";
-            log.error(msg, ex);
-            throw new IOException(msg);
-        } catch (GeneralSecurityException ex) {
-            String msg = "Something is wrong with general security while getting newTrustedTransport() for HttpTransport class.";
-            log.error(msg, ex);
-            throw new GeneralSecurityException(msg);
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (IOException ex) {
-                    log.error("Problem with I/O operation while closing file.", ex);
-                }
-            }
-        }
-    }
+		} catch (IOException ex) {
+			String msg = "Problem with I/O operation while reading google_groups.properties file.";
+			log.error(msg, ex);
+			throw new IOException(msg);
+		} catch (GeneralSecurityException ex) {
+			String msg = "Something is wrong with general security while getting newTrustedTransport() for HttpTransport class.";
+			log.error(msg, ex);
+			throw new GeneralSecurityException(msg);
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException ex) {
+					log.error("Problem with I/O operation while closing file.", ex);
+				}
+			}
+		}
+	}
 
-    /**
-     * Creates Credential object.
-     *
-     * @return an authorized Credential object.
-     */
-    private static Credential authorize() {
+	/**
+	 * Creates Credential object.
+	 *
+	 * @return an authorized Credential object.
+	 */
+	private static Credential authorize() {
 
-        try {
-            GoogleCredential credential = new GoogleCredential.Builder()
-                    .setTransport(HTTP_TRANSPORT)
-                    .setJsonFactory(JSON_FACTORY)
-                    .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-                    .setServiceAccountScopes(SCOPES)
-                    .setServiceAccountUser(USER_EMAIL)
-                    .setServiceAccountPrivateKeyFromP12File(
-                            new java.io.File(SERVICE_ACCOUNT_PKCS12_FILE_PATH))
-                    .build();
+		try {
+			GoogleCredential credential = new GoogleCredential.Builder()
+					.setTransport(HTTP_TRANSPORT)
+					.setJsonFactory(JSON_FACTORY)
+					.setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
+					.setServiceAccountScopes(SCOPES)
+					.setServiceAccountUser(USER_EMAIL)
+					.setServiceAccountPrivateKeyFromP12File(
+							new java.io.File(SERVICE_ACCOUNT_PKCS12_FILE_PATH))
+					.build();
 
-            return credential;
-        } catch (IOException ex) {
-            log.error("Problem with I/O operation while building GoogleCredential object in authorize() method.", ex);
-        } catch (GeneralSecurityException ex) {
-            log.error("Problem with security while building GoogleCredential object in authorize() method.", ex);
-        }
+			return credential;
+		} catch (IOException ex) {
+			log.error("Problem with I/O operation while building GoogleCredential object in authorize() method.", ex);
+		} catch (GeneralSecurityException ex) {
+			log.error("Problem with security while building GoogleCredential object in authorize() method.", ex);
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * Build and returns a Directory service object authorized with the service
-     * accounts that act on behalf of the given user.
-     *
-     * @return Directory service object that is ready to make requests.
-     */
-    public Directory getDirectoryService() {
+	/**
+	 * Build and returns a Directory service object authorized with the service
+	 * accounts that act on behalf of the given user.
+	 *
+	 * @return Directory service object that is ready to make requests.
+	 */
+	public Directory getDirectoryService() {
 
-        Directory service = new Directory.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize())
-                .setApplicationName(APPLICATION_NAME).build();
+		Directory service = new Directory.Builder(HTTP_TRANSPORT, JSON_FACTORY, authorize())
+				.setApplicationName(APPLICATION_NAME).build();
 
-        return service;
-    }
+		return service;
+	}
 }
